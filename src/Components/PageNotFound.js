0ee47e507d002c3c0c84bd17fdfc09984a718257
Ayoola David <ayooladavid@gmail.com>
2020-04-28 22:21:45 +0100

    first commit

---
+++ src/Components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PageNotFound = () => <div><h1> Oops! </h1><br/><h2> Page not found.</h2></div>
+
+export default PageNotFound
---
+++ test.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+process.env.NODE_ENV = 'development'
+
+module.export = {
+	mode: 'development',
+	target: 'web',
+	devTools: '',
+	entry: '',
+
+}
