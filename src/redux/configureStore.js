b30691690ff518472290ccd9fa4eeff80cffa76e
Ayoola David <ayooladavid@gmail.com>
2020-04-29 00:24:02 +0100

    development, baby !!

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './reducers'
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import { createStore, applyMiddleware, compose } from "redux"
+import rootReducer from "./reducers"
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant"
 
-export default function configureStore(initialState){
-	//adds support for redux dev tools
-	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-	return createStore(
-		rootReducer,
-		initialState, 
-		composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
-	)
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
 }
