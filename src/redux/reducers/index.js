d4f4fcd28dfcf9263eabf61df5b8e1120d073416
Ayoola David <ayooladavid@gmail.com>
2020-04-28 23:39:04 +0100

    development baby

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import courses from './'
 
-const rootReducer = combineReducers({
-	courses
-})
+const rootReducer = combineReducers({courses})
 
 export default rootReducer
