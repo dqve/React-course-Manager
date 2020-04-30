31f5e729cdfb5c875488f5c7d0cd5c6828dba028
Ayoola David <ayooladavid@gmail.com>
2020-04-30 02:18:45 +0100

    More development

--- src/Components/Courses/CoursesPage.js
+++ src/Components/Courses/CoursesPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
   
