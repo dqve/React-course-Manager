2a005997b8e465f08b4624e96e14e64ea2429175
Ayoola David <ayooladavid@gmail.com>
2020-05-02 00:50:10 +0100

    add course action thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -21,3 +21,19 @@ export function loadCourses() {
       })
   }
 }
+
+export function saveCourse(course) {
+  //eslint-disable-next-line no-unused-vars
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
+      })
+  }
+}
